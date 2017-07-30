a34fadf2f798a63a3b3e9f2eba8e75203d17d895
bernardolins <bernardolins28@gmail.com>
2017-07-30 20:22:57 -0300

    Create tests for Metric.metric_from_scope

--- test/structures/metric_test.exs
+++ test/structures/metric_test.exs
@@ -39,4 +39,27 @@ defmodule Alchemetrics.MetricTest do
       assert List.last(metrics_array).scope == :spiral
     end
   end
+
+  describe "#metric_from_scope" do
+    test "returns nil if scope is not valid" do
+      assert is_nil(Metric.metric_from_scope(:invalid, 99))
+      assert is_nil(Metric.metric_from_scope(:invalid, 95))
+      assert is_nil(Metric.metric_from_scope(:invalid, :mean))
+      assert is_nil(Metric.metric_from_scope(:invalid, :count))
+      assert is_nil(Metric.metric_from_scope(:invalid, :one))
+    end
+
+    test "returns nil if datapoint is not valid" do
+      assert is_nil(Metric.metric_from_scope(:spiral, :invalid))
+      assert is_nil(Metric.metric_from_scope(:histogram, :invalid))
+    end
+
+    test "returns the equivalent metric given the exometer datapoint and scope" do
+      assert Metric.metric_from_scope(:histogram, 99) == :p99
+      assert Metric.metric_from_scope(:histogram, 95) == :p95
+      assert Metric.metric_from_scope(:histogram, :mean) == :avg
+      assert Metric.metric_from_scope(:spiral, :count) == :total
+      assert Metric.metric_from_scope(:spiral, :one) == :last_interval
+    end
+  end
 end
