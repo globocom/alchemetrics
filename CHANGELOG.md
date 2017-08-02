09a4450d67d62a651f7c55cb3057d74a7ccf7cd4
bernardolins <bernardolins28@gmail.com>
2017-08-02 15:20:49 -0300

    Release 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.2.1 (2017-08-02)
+- Remove lager as direct dependency
+
 ## 0.2.0 (2017-08-01)
 - Creating cleaner interface for reporters
--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@
   def project do
     [
       app: :alchemetrics,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
