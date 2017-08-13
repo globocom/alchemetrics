ce6a0183c458f00ec1af1203528c4f9ccc0bc892
bernardolins <bernardolins28@gmail.com>
2017-08-13 14:42:22 -0300

    Fix LoggerReporter to use new options format

--- lib/alchemetrics/reporters/logger_reporter.ex
+++ lib/alchemetrics/reporters/logger_reporter.ex
@@ -4,10 +4,12 @@ defmodule Alchemetrics.LoggerReporter do
 
   def init(options) do
     Logger.debug "Starting #{__MODULE__} with following options: #{inspect options}"
+    {:ok, options}
   end
 
   def report(public_name, metric, value, options) do
     Logger.debug "Reporting: #{inspect %{name: public_name, metric: metric, value: value, options: options}}"
+    {:ok, options}
   end
 end
 
