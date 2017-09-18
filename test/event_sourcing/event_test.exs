c93041dbcbc2beb5e27af07c649784f8fed2d12d
bernardolins <bernardolins28@gmail.com>
2017-09-18 19:49:16 -0300

    Fix event and metric tests

--- test/event_sourcing/event_test.exs
+++ test/event_sourcing/event_test.exs
@@ -4,56 +4,50 @@ defmodule Alchemetrics.EventTest do
   alias Alchemetrics.Event
 
   describe "#create" do
-    test "name and metrics must be defined" do
-      event = Event.create(%{name: "some_event", metrics: [:p99]})
-      assert event.name == "some_event"
-      assert event.metrics == [:p99]
-      assert event.metadata == %{}
-    end
-
-    test "sets value as 1 if no value is provided" do
-      event = Event.create(%{name: "some_event", metrics: [:p99]})
-      assert event.value == 1
+    test "name and datapoints must be defined" do
+      event = Event.create(%{value: 1, datapoints: [:p99]})
+      assert event.datapoints == [:p99]
+      assert event.metadata == []
     end
 
     test "sets metadata as an empty map if no metadata is provided" do
-      event = Event.create(%{name: "some_event", metrics: [:p99]})
-      assert event.metadata == %{}
+      event = Event.create(%{value: 1, datapoints: [:p99]})
+      assert event.metadata == []
     end
 
-    test "raise ArgumentError if the name is not provided" do
+    test "raise ArgumentError if the is not provided" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{metrics: [:p99]})
+        Event.create(%{datapoints: [:p99]})
       end
     end
 
     test "raise ArgumentError if the name is not a string" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{name: 1, metrics: [:p99]})
+        Event.create(%{name: 1, datapoints: [:p99]})
       end
     end
 
-    test "raise ArgumentError if the metrics list is not provided" do
+    test "raise ArgumentError if the datapoints list is not provided" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{name: "some_event"})
+        Event.create(%{value: 1})
       end
     end
 
-    test "raise ArgumentError if the metrics list is not a atom list" do
+    test "raise ArgumentError if the datapoints list is not a atom list" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{name: "some_event", metrics: "some_invalid_metric"})
+        Event.create(%{value: 1, datapoints: "some_invalid_metric"})
       end
     end
 
     test "raise ArgumentError if the value is not a number" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{name: "some_event", metrics: [:p99], value: "one"})
+        Event.create(%{value: 1, datapoints: [:p99], value: "one"})
       end
     end
 
-    test "raise ArgumentError if metadata is not a map" do
+    test "raise ArgumentError if metadata is not a keyword list" do
       assert_raise ArgumentError, fn ->
-        Event.create(%{name: "some_event", metrics: [:p99], metadata: [invalid: true]})
+        Event.create(%{value: 1, datapoints: [:p99], metadata: %{invalid: true}})
       end
     end
   end
