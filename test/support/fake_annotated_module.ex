85e843cd3533ae8963bcb1a2ddab5d6567f0e574
bernardolins <bernardolins28@gmail.com>
2017-11-10 01:18:45 +0000

    Fix annotated functions breaking with Elixit 1.4

--- test/support/fake_annotated_module.ex
+++ test/support/fake_annotated_module.ex
@@ -2,14 +2,14 @@ defmodule FakeAnnotatedModule do
   use Alchemetrics.Annotation
 
   @alchemetrics instrument_function: true
-  def annotated_function, do: nil
+  def annotated_function, do: 100
 
   @alchemetrics instrument_function: true
   def multiple_clauses(a) when a > 3, do: a*2
   def multiple_clauses(a), do: a*4
 
   @alchemetrics instrument_function: true
-  def recursive_function([]), do: nil
+  def recursive_function([]), do: :ok
   def recursive_function([_|t]), do: recursive_function(t)
 
   @alchemetrics instrument_function: true
